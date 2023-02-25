62d0ade5827561ea468364dc85e25fe2eb33e95a
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-02-25 20:44:56 +0000

    update

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
