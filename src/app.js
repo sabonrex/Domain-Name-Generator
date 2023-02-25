8f31bf45041f2d3036ff6bef12a9c4b35057e483
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-02-25 23:29:50 +0000

    corrected

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our"];
+let colour = ["the", "our", "their"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "hers"];
+  let adj = ["great", "big", "tiny", "medium"];
+  let noun = ["tulip", "daisy", "orquit", "rose"];
+
+  let dominio = "";
+
+  for (let i = 0; i < colour.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let k = 0; k < noun.length; k++) {
+        dominio = dominio + pronoun[i] + adj[x] + noun[k] + ".com\n";
+      }
+    }
+  }
+
+  console.log(dominio);
 };
